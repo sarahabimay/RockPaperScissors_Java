b43c43635e8077f95e6d016ca12298ded45507ef
sarahabimay <sarahabimay@gmail.com>
2015-11-19 12:30:35 +0000

    Extracted methods out to check for presence of particular Symbol.

--- src/Rules.java
+++ src/Rules.java
@@ -1,27 +1,30 @@
 public class Rules {
     public SYMBOL decideWinner(SYMBOL symbol1, SYMBOL symbol2) {
-        if (symbol1 == SYMBOL.PAPER && symbol2 == SYMBOL.SCISSORS){
-            return SYMBOL.SCISSORS;
-        }
-        if (symbol1 == SYMBOL.SCISSORS && symbol2 == SYMBOL.PAPER){
-            return SYMBOL.SCISSORS;
-        }
-        if (symbol1 == SYMBOL.PAPER && symbol2 == SYMBOL.ROCK){
-            return SYMBOL.PAPER;
-        }
-        if (symbol1 == SYMBOL.ROCK && symbol2 == SYMBOL.PAPER){
-            return SYMBOL.PAPER;
-        }
-        if (symbol1 == SYMBOL.SCISSORS && symbol2 == SYMBOL.ROCK){
-            return SYMBOL.ROCK;
-        }
-        if (symbol1 == SYMBOL.ROCK && symbol2 == SYMBOL.SCISSORS){
-            return SYMBOL.ROCK;
-        }
-        if (symbolsAreTheSame(symbol1, symbol2)){
+        if (symbolsAreTheSame(symbol1, symbol2)) {
             return symbol1;
         }
+        if (isPaper(symbol1, symbol2) && isScissor(symbol1, symbol2)) {
+            return SYMBOL.SCISSORS;
+        }
+        if (isPaper(symbol1, symbol2)  && isRock(symbol1, symbol2)) {
+            return SYMBOL.PAPER;
+        }
+        if (isScissor(symbol1, symbol2) && isRock(symbol1, symbol2)) {
+            return SYMBOL.ROCK;
+        }
         return null;
+    }
+
+    private boolean isRock(SYMBOL symbol1, SYMBOL symbol2) {
+        return symbol1 == SYMBOL.ROCK || symbol2 == SYMBOL.ROCK;
+    }
+
+    private boolean isScissor(SYMBOL symbol1, SYMBOL symbol2) {
+        return symbol1 == SYMBOL.SCISSORS || symbol2 == SYMBOL.SCISSORS;
+    }
+
+    private boolean isPaper(SYMBOL symbol1, SYMBOL symbol2) {
+        return symbol1 == SYMBOL.PAPER || symbol2 == SYMBOL.PAPER;
     }
 
     private boolean symbolsAreTheSame(SYMBOL symbol1, SYMBOL symbol2) {
