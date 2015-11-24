b3ef3bd67c70937650bd2ea1b30ea6bc9261e2ce
sarahabimay <sarahabimay@gmail.com>
2015-11-24 18:33:46 +0000

    Redux of the RockPaperScissors kata.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,7 @@
-import java.io.BufferedInputStream;
-import java.io.PrintStream;
-
 public class Main {
     public static void main(String[] args) {
-        Game game = new Game(new Rules());
-        CLI cli = new CLI( new BufferedInputStream(System.in), new PrintStream(System.out), game);
-        cli.play();
+//        Game game = new Game(new Rules());
+//        CLI cli = new CLI( new BufferedInputStream(System.in), new PrintStream(System.out), game);
+//        cli.play();
     }
 }
---
+++ src/Throw.java
@@ -0,0 +1,17 @@
+
+public enum Throw {
+    ROCK(1),
+    PAPER(2),
+    SCISSORS(3);
+
+
+    private int identifier;
+
+    Throw(int identifier) {
+        this.identifier = identifier;
+    }
+
+    public int getIdentifier() {
+        return identifier;
+    }
+}
