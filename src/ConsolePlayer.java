9135a8904f218556e609154b65bd9b488b236254
sarahabimay <sarahabimay@gmail.com>
2015-11-24 19:37:16 +0000

    Introduce ConsolePlayer and tests

---
+++ src/ConsolePlayer.java
@@ -0,0 +1,11 @@
+public class ConsolePlayer {
+    private Throw consoleThrow;
+
+    public ConsolePlayer(Throw aThrow) {
+        this.consoleThrow = aThrow;
+    }
+
+    public Throw getThrow() {
+        return consoleThrow;
+    }
+}
