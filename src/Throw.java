e10e7124499918086eef7a6149e838803e8d2039
sarahabimay <sarahabimay@gmail.com>
2015-11-24 18:36:51 +0000

    Create Throw class and tests which represent Rock/Paper/Scissors

--- src/Throw.java
+++ src/Throw.java
@@ -1,24 +1,17 @@
-import java.util.HashMap;
-import java.util.Map;
 
 public enum Throw {
     ROCK(1),
     PAPER(2),
     SCISSORS(3);
 
-    static Map<Integer, Throw> ordinalToSymbol = new HashMap<>();
+
+    private int identifier;
 
     Throw(int identifier) {
-
+        this.identifier = identifier;
     }
 
-    public static void setOrdinalToSymbol() {
-        ordinalToSymbol.put(1, ROCK);
-        ordinalToSymbol.put(2, PAPER);
-        ordinalToSymbol.put(3, SCISSORS);
-    }
-
-    public static Throw getSymbolFromOrdinal(int ordinal) {
-        return ordinalToSymbol.get(ordinal);
+    public int getIdentifier() {
+        return identifier;
     }
 }
--- test/ThrowTest.java
+++ test/ThrowTest.java
@@ -1,2 +1,23 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ThrowTest {
+    @Test
+    public void verifyRockThrowIdentifier() {
+        Throw rockThrow = Throw.ROCK;
+        assertEquals(1, rockThrow.getIdentifier());
+    }
+
+    @Test
+    public void verifyPaperThrowIdentifier() {
+        Throw rockThrow = Throw.PAPER;
+        assertEquals(2, rockThrow.getIdentifier());
+    }
+
+    @Test
+    public void verifyScissorsThrowIdentifier() {
+        Throw rockThrow = Throw.SCISSORS;
+        assertEquals(3, rockThrow.getIdentifier());
+    }
 }
