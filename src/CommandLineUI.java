d131a9efc2fb6e7cb97e596f8cebb8dd4f3958a6
sarahabimay <sarahabimay@gmail.com>
2015-11-24 19:57:02 +0000

    Test Display User move request to command line out

--- src/CommandLineUI.java
+++ src/CommandLineUI.java
@@ -2,6 +2,7 @@ import java.io.PrintStream;
 
 public class CommandLineUI {
     public String GREETING_MESSAGE = "Welcome to the Rock Paper Scissors Game.\n\n";
+    public String CONSOLE_MOVE_REQUEST = "Please enter Rock(1), Paper(2) or Scissors(3): \n";
     private PrintStream writeStream;
 
     public CommandLineUI(PrintStream printStream) {
@@ -10,5 +11,9 @@ public class CommandLineUI {
 
     public void displayGreeting() {
         writeStream.println(GREETING_MESSAGE);
+    }
+
+    public void requestConsoleMove() {
+        writeStream.println(CONSOLE_MOVE_REQUEST);
     }
 }
--- test/CommandLineUITest.java
+++ test/CommandLineUITest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertThat;
 public class CommandLineUITest {
     private OutputStream output;
     private PrintStream printStream;
+    private String SCISSORS_THROW;
 
     @Before
     public void setUp() {
@@ -23,4 +24,13 @@ public class CommandLineUITest {
         String expected = cli.GREETING_MESSAGE;
         assertThat(output.toString(), containsString(expected));
     }
+
+    @Test
+    public void displayConsoleMoveRequest() {
+        CommandLineUI cli = new CommandLineUI(printStream);
+        cli.requestConsoleMove();
+        String expected = cli.CONSOLE_MOVE_REQUEST;
+        assertThat(output.toString(), containsString(expected));
+    }
+
 }
