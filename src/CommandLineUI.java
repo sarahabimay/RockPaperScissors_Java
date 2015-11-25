282f9f14571be82ae1cb17ab11102b84f0304928
sarahabimay <sarahabimay@gmail.com>
2015-11-25 16:55:44 +0000

    Request replay choice from command line

--- src/CommandLineUI.java
+++ src/CommandLineUI.java
@@ -12,16 +12,46 @@ public class CommandLineUI {
         this.writeStream = printStream;
     }
 
-    public Throw requestConsoleTurn() {
-        writeStream.println("Please select Rock(1), Paper(2), or Scissors(3): \n");
-        int consoleMove = readLine();
-        writeStream.println(String.format("Your move was: %s", consoleMove));
-        return convertToThrow(consoleMove).get();
+    public Optional<Throw> requestConsoleTurn() {
+        int INVALID_MOVE = 9;
+        int consoleMove = INVALID_MOVE;
+        Optional<Throw> consoleThrow = Optional.empty();
+        while (!isValidThrow(consoleMove)) {
+            writeStream.println("Please select Rock(1), Paper(2), or Scissors(3): \n");
+            consoleMove = readLine();
+            writeStream.println(String.format("Your move was: %s", consoleMove));
+            if (!isValidThrow(consoleMove)) {
+                writeStream.println("Invalid choice. \n");
+            }
+            consoleThrow = convertToThrow(consoleMove);
+        }
+        return consoleThrow;
+    }
+
+    public boolean isValidThrow(int consoleMove) {
+        return !convertToThrow(consoleMove).equals(Optional.empty());
+    }
+
+    public int requestReplay() {
+        int INVALID_CHOICE = 9;
+        int replayChoice = INVALID_CHOICE;
+        while (!isValidReplayChoice(replayChoice )) {
+            writeStream.println("Do you want to play again? Yes(1) or No(2): \n");
+            replayChoice = readLine();
+            if (!isValidReplayChoice(replayChoice)) {
+                writeStream.println("Invalid selection: \n");
+            }
+        }
+        return replayChoice;
+    }
+
+    private boolean isValidReplayChoice(int replayChoice) {
+        return replayChoice == 1 || replayChoice == 2;
     }
 
     private Optional<Throw> convertToThrow(int consoleMove) {
         for (Throw aThrow : Throw.values()) {
-            if (aThrow.equals(consoleMove)){
+            if (aThrow.equals(consoleMove)) {
                 return of(aThrow);
             }
         }
@@ -38,7 +68,4 @@ public class CommandLineUI {
         return 0;
     }
 
-    public boolean isValidThrow(int consoleMove) {
-        return !convertToThrow(consoleMove).equals(Optional.empty());
-    }
 }
--- test/CommandLineTest.java
+++ test/CommandLineTest.java
@@ -2,8 +2,11 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
+import java.util.Optional;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class CommandLineTest implements UserInterface{
     private OutputStream output;
@@ -28,5 +31,46 @@ public class CommandLineTest implements UserInterface{
         InputStream inputStream = new ByteArrayInputStream("99\n".getBytes());
         cli = new CommandLineUI(inputStream, printStream);
         assertEquals(false, cli.isValidThrow(99));
+    }
+
+    @Test
+    public void requestInvalidHumanInput() {
+        InputStream inputStream = new ByteArrayInputStream("99\n".getBytes());
+        cli = new CommandLineUI(inputStream, printStream);
+        assertEquals(Optional.empty(), cli.requestConsoleTurn());
+        assertThat(output.toString(), containsString("Invalid choice. \n"));
+    }
+
+    @Test
+    public void invalidThenValidHumanChoice() {
+        byte[] buf = "9\n2\n".getBytes();// 9 == invalid and 2 == Paper
+        InputStream inputStream = new ByteArrayInputStream(buf);
+        cli = new CommandLineUI(inputStream, printStream);
+        assertEquals(Throw.PAPER, cli.requestConsoleTurn().get());
+        assertThat(output.toString(), containsString("Invalid choice. \n"));
+    }
+
+    @Test
+    public void displayReplayChoices() {
+        InputStream inputStream = new ByteArrayInputStream("2\n".getBytes());
+        cli = new CommandLineUI(inputStream, printStream);
+        assertEquals(2, cli.requestReplay());
+        assertThat(output.toString(), containsString("Do you want to play again? Yes(1) or No(2): \n"));
+    }
+
+    @Test
+    public void replayChoiceIsInvalid() {
+        InputStream inputStream = new ByteArrayInputStream("99\n".getBytes());
+        cli = new CommandLineUI(inputStream, printStream);
+        assertEquals(99, cli.requestReplay());
+        assertThat(output.toString(), containsString("Invalid selection: \n"));
+    }
+    @Test
+    public void requestReplayChoiceTillInvalid() {
+        byte[] buf = "9\n2\n".getBytes();// 9 == invalid and 2 == No
+        InputStream inputStream = new ByteArrayInputStream(buf);
+        cli = new CommandLineUI(inputStream, printStream);
+        assertEquals(2, cli.requestReplay());
+        assertThat(output.toString(), containsString("Invalid selection: \n"));
     }
 }
