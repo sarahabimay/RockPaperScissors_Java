c5161f5c946b553e14c713eeccd7588c7cdedf5e
sarahabimay <sarahabimay@gmail.com>
2015-11-25 14:24:05 +0000

    Randomly Generate a next Throw

--- src/AIPlayer.java
+++ src/AIPlayer.java
@@ -1,5 +1,25 @@
-public class AIPlayer implements Player{
+import java.util.Random;
+
+public class AIPlayer implements Player {
+    private Throw aThrow;
+
     public Throw generateThrow() {
-        return Throw.PAPER;
+        this.aThrow = getRandomSymbolInRange(Throw.values().length);
+        return aThrow;
+    }
+
+    private Throw getRandomSymbolInRange(int range) {
+        int randomOrdinal = randomNumberInRange(randomFractionFromRange(range));
+        return Throw.values()[randomOrdinal];
+    }
+
+    private long randomFractionFromRange(long range) {
+        Random randomGenerator = new Random();
+        return (long) (range * randomGenerator.nextDouble());
+    }
+
+    private int randomNumberInRange(long fraction) {
+        int start = 1;
+        return (int) (fraction + start);
     }
 }
