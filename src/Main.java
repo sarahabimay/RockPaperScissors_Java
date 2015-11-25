9164b9a8e99a17bb3b410b2389bf2c5824547ce3
sarahabimay <sarahabimay@gmail.com>
2015-11-25 19:27:10 +0000

    Game and tests introduced.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.io.BufferedInputStream;
+import java.io.PrintStream;
+
 public class Main {
     public static void main(String[] args) {
-//        CLI cli = new CLI( new BufferedInputStream(System.in), new PrintStream(System.out), game);
-//        Game game = new Game(cli, new Rules());
-//        cli.play();
+        CommandLineUI cli = new CommandLineUI( new BufferedInputStream(System.in), new PrintStream(System.out));
+        Game game = new Game(cli, new Rules());
+        game.startGame();
     }
 }
