76918ded0efb851a4bb1898d3c2004f2079261aa
sarahabimay <sarahabimay@gmail.com>
2015-11-25 19:27:10 +0000

    # This is a combination of 9 commits. # The first commit's message is: Test console move is requested and displayed
    # This is the 2nd commit message:
    
    Create ConsolePlayer after console move is found
    
    # This is the 3rd commit message:
    
    Create AI Player
    
    # This is the 4th commit message:
    
    Game asks AIPlayer for it's move.
    
    * This test forced the refactor of Player contract to include both a getter
    and a generateThrow method
    * generateThrow will set the Player's 'throw' field
    * getThrow returns 'throw' field.
    * I felt this test also forced the refactor of ConsolePlayer such that it
    now has a generateThrow method and takes the UserInterface in it's constructor.
    generateThrow now calls out to the UserInterface for position
    
    # This is the 5th commit message:
    
    Play ConsolePlayer move and AiPlayer move against each other and display result.
    
    # This is the 6th commit message:
    
    Game plays two moves and displays Draw result
    
    # This is the 7th commit message:
    
    Remove unneccessary Game constructor and adapt any references to it.
    
    # This is the 8th commit message:
    
    Integration test, Display AI Move test and refactoring thereof.
    
    * CommandLineUI method which read in User/Console move was also displaying it.
      Split this method up into two parts.
    * Change argument types of GAme::throwPlayerMoves to take Player instead of Throw.
    
    # This is the 9th commit message:
    
    Move private GAme method into correct position

--- test/GameTest.java
+++ test/GameTest.java
@@ -2,8 +2,12 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.isIn;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class GameTest {
@@ -20,8 +24,95 @@ public class GameTest {
     public void askCommandLineToDisplayGreeting() {
         InputStream inputStream = new ByteArrayInputStream("1\n".getBytes());
         CommandLineUI cli = new CommandLineUI(inputStream, printStream);
-        Game game = new Game(cli);
+        Game game = new Game(cli, new Rules());
         game.askUIToDisplayGreeting();
         assertThat(output.toString(), containsString(cli.GREETING_PROMPT));
+    }
+
+    @Test
+    public void getAndDisplayConsoleMoveToCommandLine() {
+        InputStream inputStream = new ByteArrayInputStream("1\n".getBytes());
+        CommandLineUI cli = new CommandLineUI(inputStream, printStream);
+        Game game = new Game(cli, new Rules());
+        game.displayConsoleMove(game.createConsolePlayer().generateThrow());
+        String expected = String.format(cli.CONSOLE_MOVE, Throw.ROCK);
+        assertThat(output.toString(), containsString(expected));
+    }
+
+    @Test
+    public void checkValidAIMoveIsGenerate() {
+        InputStream inputStream = new ByteArrayInputStream("1\n".getBytes());
+        CommandLineUI cli = new CommandLineUI(inputStream, printStream);
+        Game game = new Game(cli, new Rules());
+        assertThat(game.createAIPlayer().generateThrow(),
+                isIn(Arrays.asList(Throw.ROCK, Throw.PAPER, Throw.SCISSORS)));
+    }
+
+    @Test
+    public void displayAIMoveToCommandLine() {
+        InputStream inputStream = new ByteArrayInputStream("1\n".getBytes());
+        CommandLineUI cli = new CommandLineUI(inputStream, printStream);
+        Game game = new Game(cli, new Rules());
+        FakeAIPlayer aiPlayer = generateFakeAIPlayerAndMove(Throw.PAPER);
+        Throw aiMove = aiPlayer.generateThrow();
+        game.displayAIMove(aiMove);
+        String expected = String.format(cli.AI_MOVE, Throw.PAPER);
+        assertThat(output.toString(), containsString(expected));
+    }
+
+    @Test
+    public void confirmTwoPlayersAddedToGame() {
+        InputStream inputStream = new ByteArrayInputStream("1\n".getBytes());
+        CommandLineUI cli = new CommandLineUI(inputStream, printStream);
+        Game game = new Game(cli, new Rules());
+        game.createConsolePlayer();
+        game.createAIPlayer();
+        assertEquals(2, game.getPlayers().size());
+    }
+
+    @Test
+    public void playTheGameAndDisplayWinningResult() {
+        InputStream inputStream = new ByteArrayInputStream("1\n".getBytes());
+        CommandLineUI cli = new CommandLineUI(inputStream, printStream);
+        Game game = new Game(cli, new Rules());
+        ConsolePlayer consolePlayer = generateConsolePlayerAndMove(cli);
+        FakeAIPlayer aiPlayer = generateFakeAIPlayerAndMove(Throw.PAPER);
+        Optional<Throw> result = game.throwPlayerMoves(consolePlayer, aiPlayer);
+        game.askUIToDisplayResult(result);
+        String expected = String.format(cli.WINNING_RESULT, Throw.PAPER);
+        assertThat(output.toString(), containsString(expected));
+    }
+
+    @Test
+    public void playTheGameAndDisplayDrawResult() {
+        InputStream inputStream = new ByteArrayInputStream("1\n".getBytes());
+        CommandLineUI cli = new CommandLineUI(inputStream, printStream);
+        Game game = new Game(cli, new Rules());
+        ConsolePlayer consolePlayer = generateConsolePlayerAndMove(cli);
+        FakeAIPlayer aiPlayer = generateFakeAIPlayerAndMove(Throw.ROCK);
+        Optional<Throw> result = game.throwPlayerMoves(consolePlayer, aiPlayer);
+        game.askUIToDisplayResult(result);
+        assertThat(output.toString(), containsString(cli.ANNOUNCE_DRAW));
+    }
+
+    @Test
+    public void gameIntegrationTest() {
+        InputStream inputStream = new ByteArrayInputStream("1\n".getBytes());
+        CommandLineUI cli = new CommandLineUI(inputStream, printStream);
+        Game game = new Game(cli, new Rules());
+        game.startGame();
+        assertThat(output.toString(), containsString(cli.GAME_OVER));
+    }
+
+    private FakeAIPlayer generateFakeAIPlayerAndMove(Throw dummyMove) {
+        FakeAIPlayer aiPlayer = new FakeAIPlayer();
+        aiPlayer.nextThrow(dummyMove);
+        return aiPlayer;
+    }
+
+    private ConsolePlayer generateConsolePlayerAndMove(CommandLineUI cli) {
+        ConsolePlayer consolePlayer = new ConsolePlayer(cli);
+        consolePlayer.generateThrow();
+        return consolePlayer;
     }
 }
